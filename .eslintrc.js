7bba60cbf1eba37b5ed617e03914108795d37dc4
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-10-12 18:10:21 +0200

    changed eslint rules for deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
-  parserOptions: {
-    ecmaVersion: 2022, 
-  },
   plugins: ["prettier"],
   extends: ["plugin:prettier/recommended"],
   rules: {
-    
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
   },
 };
