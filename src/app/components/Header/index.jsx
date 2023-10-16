0117d29d5c166d1e388fd2ff077f2ab2d1279713
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-10-16 11:08:12 +0200

    add link to logo

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <>
+    <Link href= "https://jolint.com/" target="_blank">
       <Image
         src="/JOLINT-logo.svg.jpg"
+
         width={120}
         height={35.518}
         alt="Jolint logo"
-      />
+        />
+    </Link>
     </>
   )
 }
