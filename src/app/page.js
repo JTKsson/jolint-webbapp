7f9f66d244448fb4ee173bcf1db92da7e29edada
CyberGalactus <edu.garma97@hotmail.com>
2023-10-19 14:12:44 +0200

    navbar p-48

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
     </>
-  )
-}
+  );
+};
