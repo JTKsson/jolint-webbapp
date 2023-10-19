902d0204fd89a2702899e9f090687b7f93b111a8
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-10-19 10:49:32 +0200

    added about us page

--- src/app/components/About/index.jsx
+++ src/app/components/About/index.jsx
@@ -17,39 +17,40 @@ function About() {
         inclusive and equitable work culture for all.
       </p>
       <h2>Co-founders:</h2>
-      <div className="founder">
-        <img/>
+      <div>
+      <img src="/Klaudia.jpg" alt="Klaudia Mur" />
         <h3>Klaudia Mur</h3>
         <p>
           I am a data scientist and physicist. My background in Complex Adaptive
           Systems has fueled my passion for understanding how people interact,
           communicate, and collaborate in the workplace. My mission is to use
-          data-driven analysis to make the workplace a better place for employees,
-          by understanding the complexities of human interactions and finding ways
-          to improve them. Join me on my journey to enhance workplace culture and
-          improve the work environment. With my expertise in data analysis and
-          understanding of human behavior, I am committed to leading the way
-          towards a more inclusive and equitable work culture for all.
+          data-driven analysis to make the workplace a better place for
+          employees, by understanding the complexities of human interactions and
+          finding ways to improve them. Join me on my journey to enhance
+          workplace culture and improve the work environment. With my expertise
+          in data analysis and understanding of human behavior, I am committed
+          to leading the way towards a more inclusive and equitable work culture
+          for all.
         </p>
       </div>
-      <div className="founder">
-        <img/>
+      <div>
+      <img src="/Faheem.jpg" alt="Faheem Shah" />
         <h3>Faheem Shah</h3>
         <p>
           As the co-founder of Jolint, I bring a wealth of expertise in
           organizational communication and collaboration to the table. With a
           background in international politics, communication studies, and
           specialized training in these areas, I understand the intricacies and
-          nuances of effective communication within organizations. My passion for
-          this field led me to start Jolint, with the goal of empowering companies
-          to create more inclusive and equitable work cultures. Join me in my
-          journey with Jolint, as we work together to improve communication and
-          collaboration within organizations for a more inclusive work
-          environment.
+          nuances of effective communication within organizations. My passion
+          for this field led me to start Jolint, with the goal of empowering
+          companies to create more inclusive and equitable work cultures. Join
+          me in my journey with Jolint, as we work together to improve
+          communication and collaboration within organizations for a more
+          inclusive work environment.
         </p>
       </div>
     </div>
-  );
+  )
 }
 
-export default About;
+export default About
