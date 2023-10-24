65197f5187ec632aabfd6327f1b3f6fa95bd632f
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-10-24 16:00:45 +0200

    added title to content

--- src/app/components/Advantages/index.jsx
+++ src/app/components/Advantages/index.jsx
@@ -1,12 +1,17 @@
 import { AdvantagesContent } from '@/assets/advantages-content'
-import Styles from "./advantages.module.css"
+import Styles from './advantages.module.css'
 
 const Advantages = () => {
   return (
-    <div className={Styles.contentBox}>
+    <div className={Styles.advantages}>
+      <h2>Advantages with Jolint at your workplace</h2>
+      <div className={Styles.contentBox}>
         {AdvantagesContent.map((item, index) => (
-          <p className={Styles.contentCard} key={index}>{item.content}</p>
+          <p className={Styles.contentCard} key={index}>
+            {item.content}
+          </p>
         ))}
+      </div>
     </div>
   )
 }
