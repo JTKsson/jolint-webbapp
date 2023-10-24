5f6819d6d4edfc40cd4548dfd939c991bc04b77a
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-10-24 16:56:07 +0200

    merge p-131 advantages to main

---
+++ src/app/assets/heroContent.jsx
@@ -0,0 +1,4 @@
+export const HeroContent = {  
+  title: "Let’s Create an Inclusive Workspace Together",
+  content: "Gain valuable data insights with our solution that measures inclusion and belonging within your organization, enabling you to take action for a more inclusive and vibrant work culture while maintaining the privacy and integrity of your personal data",
+}
