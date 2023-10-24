174e90245a0236fe271a51ac37abb56674a86699
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-10-24 12:01:53 +0200

    add hero to project

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar"
+import HeroComponent from "./components/HeroComponent"
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <HeroComponent/>
     </>
   )
 }
