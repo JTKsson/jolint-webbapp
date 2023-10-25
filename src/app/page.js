de82c64afaa9b7fd1ca4602fbd98d50d02c2dcb0
CyberGalactus <edu.garma97@hotmail.com>
2023-10-25 14:25:24 +0200

    p-88 how it works page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import HowItWorks from "./components/HowItWorks"
+import { HowItWorksContent } from "./components/HowItWorks/HowItWorksContent"
 import Navbar from "./components/Navbar"
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <HowItWorks data={HowItWorksContent}/>
     </>
   )
 }
