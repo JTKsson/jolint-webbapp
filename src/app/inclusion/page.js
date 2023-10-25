d9b7f99eff1ffe4a53acdf5c1320f6a008ec8bad
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-10-25 16:17:42 +0200

    added menatal health components

--- src/app/inclusion/page.js
+++ src/app/inclusion/page.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import mentalHealth from '../assets/mental-health'
 
-const MentalHealth = () => {
+const InclusionPage = () => {
   return (
     <div>
+      <h3>About mental health at work</h3>
       {mentalHealth.map((item, index) => (
         <div key={index}>
           <h3>{item.title}</h3>
           <p>{item.content}</p>
-          <p>{item.readmore}</p>
         </div>
       ))}
     </div>
   )
 }
 
-export default MentalHealth
+export default InclusionPage
