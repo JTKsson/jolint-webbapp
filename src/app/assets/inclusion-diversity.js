4b596f18dc7203c22fbd0b4895fbf41a78bfd836
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-10-25 11:40:30 +0200

    added the content of inclusion diversity

---
+++ src/app/assets/inclusion-diversity.js
@@ -0,0 +1,18 @@
+const inclusionDiversity = [
+    {
+        title:'How diversity helps in work',
+        content:' By welcoming individuals from diverse backgrounds, experiences, and perspectives, businesses open doors to a wealth of fresh ideas and insights.',
+        readmore:'...'
+    },
+    {
+        title:'Creating an inclusive culture',
+        content:'Show solidarity with marginalized groups and actively support their inclusion. Advocate for equal opportunities, fair treatment, and representation in decision-making processes.',
+        readmore:'...'
+    },
+    {
+        title:'The power of diversity',
+        content:'Appreciate and value the diverse backgrounds, experiences, and perspectives of your colleagues. Engage in conversations and initiatives that celebrate diversity, diversity-focused training programs.',
+        readmore:'...'
+    }
+]
+export default inclusionDiversity;
