d24afc0a364d14d000c43db962d24a5b88dbfde2
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-10-25 15:57:09 +0200

    removed read more after changes in design

--- src/app/assets/mental-health.js
+++ src/app/assets/mental-health.js
@@ -3,19 +3,16 @@ const mentalHealth = [
     title: 'Creating a supportive work environment',
     content:
       'Be willing to lend a hand to your colleagues when they need assistance. Collaborate and share knowledge and resources to foster a sense of teamwork.',
-    readmore: '...',
   },
   {
     title: 'Addressing mental health in the workplace',
     content:
       'Offer support during challenging times. Be there for your coworkers during difficult periods, such as personal crises or work-related challenges.',
-    readmore: '...',
   },
   {
     title: 'Work-life balance',
     content:
       'Promote work-life balance. Encourage a healthy work-life balance among your peers. Respect personal boundaries and avoid unnecessarily pressuring colleagues to work beyond their capacity.',
-    readmore: '...',
   },
 ]
-export default mentalHealth;
+export default mentalHealth
