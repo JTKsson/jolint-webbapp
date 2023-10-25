0cd9c1acbe7e1e3f98bbf3dc892120915579b1d8
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-10-25 11:46:01 +0200

    added inclusion diversity

--- src/app/inclusion/page.js
+++ src/app/inclusion/page.js
@@ -1,8 +1,18 @@
+import React from 'react'
+import inclusionDiversity from '../assets/inclusion-diversity'
 
-const inclusionPage = () => {
+const inclusion = () => {
   return (
-    <h2>Inclusion page</h2>
+    <div>
+      {inclusionDiversity.map((item, index) => (
+        <div key={index}>
+          <h3>{item.title}</h3>
+          <p>{item.content}</p>
+          <p>{item.readmore}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default inclusionPage
+export default inclusion;
