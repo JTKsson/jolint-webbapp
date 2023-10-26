186c709ded95c6fe2cf7e828f942634e45b6c1ac
CyberGalactus <edu.garma97@hotmail.com>
2023-10-26 14:11:28 +0200

    p-139 inclusion page title

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroComponent from "./components/HeroComponent"
+import InclusionPage from "./components/InclusionPageTitle"
 import Advantages from "./components/Advantages"
 
 export default function Home() {
   return (
     <>
       <HeroComponent/>
+      <InclusionPage />
       <Advantages/>
     </>
   )
