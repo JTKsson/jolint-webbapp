6073c2024cdb7a482fe97aed4c5f3d75445ac9b9
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-10-30 13:56:57 +0100

    add styling to advantages component

--- src/app/components/Advantages/index.jsx
+++ src/app/components/Advantages/index.jsx
@@ -4,12 +4,12 @@ import Styles from './advantages.module.css'
 const Advantages = () => {
   return (
     <div className={Styles.advantages}>
-      <h2>Advantages with Jolint at your workplace</h2>
+      <h2 className={Styles.title}>Advantages with Jolint at your workplace</h2>
       <div className={Styles.contentBox}>
         {AdvantagesContent.map((item, index) => (
-          <p className={Styles.contentCard} key={index}>
-            {item.content}
-          </p>
+          <div className={Styles.contentCard}>
+            <p key={index}>{item.content}</p>
+          </div>
         ))}
       </div>
     </div>
