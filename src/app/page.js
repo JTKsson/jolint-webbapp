b12ac574b5528d4ef85745460fd6426d764abb87
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-10-30 11:39:15 +0100

    removed doubled component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import HeroComponent from "./components/HeroComponent"
 import Advantages from "./components/Advantages"
-import InclusionStats from "./components/InclusionStats"
 import HowItWorks from "./components/HowItWorks"
 import { HowItWorksContent } from "./components/HowItWorks/HowItWorksContent"
 
@@ -9,7 +8,6 @@ export default function Home() {
     <>
       <HeroComponent/>
       <Advantages/>
-      <InclusionStats/>
       <HowItWorks data={HowItWorksContent}/>
     </>
   )
