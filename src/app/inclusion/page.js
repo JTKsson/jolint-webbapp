9f1963de138fc78fd346050054f3ce3bf54efd23
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-11-01 15:02:37 +0100

    fixed conflict

--- src/app/inclusion/page.js
+++ src/app/inclusion/page.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-import InclusionDropdownComponent from '../components/InclusionDropdownComponent'
-=======
 import InclusionDropdownComponent from "../components/InclusionDropdownComponent"
->>>>>>> bugfix/p-187-remove-double-inclusion-stats
 
 const inclusionPage = () => {
   return (
     <>
     <h2>Inclusion page</h2>
-<<<<<<< HEAD
-    <InclusionDropdownComponent />
-=======
     <InclusionDropdownComponent/>
->>>>>>> bugfix/p-187-remove-double-inclusion-stats
     </>
   )
 }
