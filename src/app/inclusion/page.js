bc501ff15fafdb7b0750b08307e6389d46205e74
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-11-02 11:16:42 +0100

    removed readmore

--- src/app/inclusion/page.js
+++ src/app/inclusion/page.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import inclusionDiversity from '../assets/inclusion-diversity'
 
-const inclusion = () => {
+const InclusionPage = () => {
   return (
     <div>
       {inclusionDiversity.map((item, index) => (
         <div key={index}>
           <h3>{item.title}</h3>
           <p>{item.content}</p>
-          <p>{item.readmore}</p>
         </div>
       ))}
     </div>
   )
 }
 
-export default inclusion;
+export default InclusionPage;
