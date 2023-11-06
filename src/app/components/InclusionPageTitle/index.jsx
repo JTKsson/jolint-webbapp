09c727a70e9c99c2d6f7d21c08876ee80b21f83a
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-11-06 15:37:20 +0100

    add styling to inclusion stats

--- src/app/components/InclusionPageTitle/index.jsx
+++ src/app/components/InclusionPageTitle/index.jsx
@@ -1,32 +1,34 @@
 'use client'
 import CountUp from 'react-countup'
+import Styles from "./inclusionPageTitle.module.css"
+
 const InclusionPage = () => {
   return (
-    <>
-      <h1>Inclusion makes a difference</h1>
-      <p>
+    <div className={Styles.inclusionStats}>
+      <h1 className={Styles.title}>Inclusion makes a difference</h1>
+      <p className={Styles.subTitle}>
         Organisations with higher inclusion among coworkers have according to
         [source]:{' '}
       </p>
-      <div className="animation">
-        <div className="percentage">
-          <CountUp end={31} duration={7} suffix="%" />
-          <p>Less isolation</p>
+      <div className={Styles.contentBox}>
+        <div className={Styles.contentCard}>
+          <CountUp className={Styles.number} end={31} duration={7} suffix="%" />
+          <p className={Styles.statName}>Less isolation</p>
         </div>
-        <div className="percentage">
-          <CountUp end={41} duration={7} suffix="%" />
-          <p>Lower sick leave</p>
+        <div className={Styles.contentCard}>
+          <CountUp className={Styles.number} end={41} duration={7} suffix="%" />
+          <p className={Styles.statName}>Lower sick leave</p>
         </div>
-        <div className="percentage">
-          <CountUp end={59} duration={7} suffix="%" />
-          <p>Lower staff turnover</p>
+        <div className={Styles.contentCard}>
+          <CountUp className={Styles.number} end={59} duration={7} suffix="%" />
+          <p className={Styles.statName}>Lower staff turnover</p>
         </div>
-        <div className="percentage">
-          <CountUp end={61} duration={7} suffix="%" />
-          <p>More satisfied employees</p>
+        <div className={Styles.contentCard}>
+          <CountUp className={Styles.number} end={61} duration={7} suffix="%" />
+          <p className={Styles.statName}>More satisfied employees</p>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 export default InclusionPage
