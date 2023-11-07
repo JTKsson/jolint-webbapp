ee49794277def008cdbaebf9dabc0268fde9fba0
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-11-07 12:19:42 +0100

    base style done

--- src/app/components/HowItWorks/index.jsx
+++ src/app/components/HowItWorks/index.jsx
@@ -1,19 +1,21 @@
-"use client"
+'use client'
+import Styles from './howItWorks.module.css'
 
-const HowItWorks = ({data}) => {
-    return (
-        <>
-        <h1>How it works</h1>
-        {data.map((item, index) => (
-            <section key={index}>
-                <img className="image" src={item.image}/>
-                <div className="textContent">
-                    <h2>{item.title}</h2>
-                    <p>{item.content}</p>
-                </div>
-            </section>
-        ))}
-        </>
-    );
-};
-export default HowItWorks;
+const HowItWorks = ({ data }) => {
+  return (
+    <div className={Styles.contentBox}>
+      <h1 className={Styles.mainTitle}>How it works</h1>
+      {data.map((item, index) => (
+        <section className={Styles.contentCard} key={index}>
+          <img className={Styles.image} src={item.image} />
+          <div className={Styles.eclipse}></div>
+          <div className={Styles.cardContent}>
+            <h2 className={Styles.cardTitle}>{item.title}</h2>
+            <p className={Styles.textContent}>{item.content}</p>
+          </div>
+        </section>
+      ))}
+    </div>
+  )
+}
+export default HowItWorks
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import HeroComponent from "./components/HeroComponent"
 import Advantages from "./components/Advantages"
 import HowItWorks from "./components/HowItWorks"
-import { HowItWorksContent } from "./components/HowItWorks/HowItWorksContent"
+import { HowItWorksContent } from "./assets/HowItWorksContent"
 
 export default function Home() {
   return (
