4ddaf9bcc1131ece535e045cc984747c064f8cbb
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-11-11 19:00:38 +0100

    Update data

--- src/app/assets/faq-content.js
+++ src/app/assets/faq-content.js
@@ -1,27 +1,27 @@
 const faqContent = [
   {
-    Q: 'Q : Why do I need to sign a consent form?',
-    A: 'A : To be able to help your organisation to improve inclusion and belonging, also create a more diverse and inclusive workplace, we at Jolint need your consent to collect your personal data.',
+    Q: 'Q: Why do I need to sign a consent form?',
+    A: 'A: To be able to help your organisation to improve inclusion and belonging, also create a more diverse and inclusive workplace, we at Jolint need your consent to collect your personal data.',
   },
   {
-    Q: 'Q : How does AI analyze the data?',
-    A: 'A : The algorithms look for patterns and relationships in the data, and will explore gender and age differences, uncovering variations in social influence and work patterns.',
+    Q: 'Q: How does AI analyze the data?',
+    A: 'A: The algorithms look for patterns and relationships in the data, and will explore gender and age differences, uncovering variations in social influence and work patterns.',
   },
   {
-    Q: 'Q : Do you read my emails and chats?',
-    A: 'A : The content of communications and calendars is never viewed nor processed. Jolint will only be processing the metadata associated with those communication channels.',
+    Q: 'Q: Do you read my emails and chats?',
+    A: 'A: The content of communications and calendars is never viewed nor processed. Jolint will only be processing the metadata associated with those communication channels.',
   },
   {
-    Q: 'Q : What result will I see?',
-    A: 'A : You will access your team result as well as the result of the organisation.',
+    Q: 'Q: What result will I see?',
+    A: 'A: You will access your team result as well as the result of the organisation.',
   },
   {
-    Q: 'Q : How can inclusion help me at my workplace?',
-    A: 'A :Research shows that diverse companies are more innovative and productive. A wide range of perspectives leads to a larger pool of knowledge and experiences to choose from, resulting in better decision making and problem solving.',
+    Q: 'Q: How can inclusion help me at my workplace?',
+    A: 'A: Research shows that diverse companies are more innovative and productive. A wide range of perspectives leads to a larger pool of knowledge and experiences to choose from, resulting in better decision making and problem solving.',
   },
   {
-    Q: 'Q : How long will you save my data?',
-    A: 'A : The data will be stored as long as Jolint has a collaboration with your organisation. This will make it possible to look for patterns on monthy and yearly basis. With that said, all your data will be anonymized and can not be lead to you.',
+    Q: 'Q: How long will you save my data?',
+    A: 'A: The data will be stored as long as Jolint has a collaboration with your organisation. This will make it possible to look for patterns on monthy and yearly basis. With that said, all your data will be anonymized and can not be lead to you.',
   },
 ]
 
