73aac4df374ce4548ed64753cee7e597a6d6a6f2
CyberGalactus <edu.garma97@hotmail.com>
2023-11-13 15:41:06 +0100

    fixed styling and mobile view responsiveness

--- src/app/components/InclusionDropdownComponent/index.jsx
+++ src/app/components/InclusionDropdownComponent/index.jsx
@@ -18,7 +18,7 @@ const ContentCard = ({ title, imageSrc, content }) => {
     <div className={Styles.cardContent}>
       <div className={Styles.cardHeader} onClick={handleCardClick}>
         <Image src={imageSrc} width={349} height={266} alt="" />
-        <h2>{title}</h2>
+        <p className={Styles.cardTitle}>{title}</p>
       </div>
       
       {expand && <div className={Styles.cardExtended} onMouseLeave={handleMouseLeave}>{content}</div>}
@@ -32,43 +32,50 @@ const InclusionDropdownComponent = ({
   mentalHealth,
 }) => {
   return (
-    <>
-      <h2 className={Styles.title2}>About inclusion at workplace</h2>
-      <div className={Styles.container1}>
-        {inclusionContent.map((item, index) => (
-          <ContentCard
-            key={`inclusion-${index}`}
-            title={item.title}
-            content={item.content}
-            imageSrc={item.imageSrc}
-          />
-        ))}
-      </div>
+    <div className={Styles.inclusionBody}>  
 
-      <h2 className={Styles.title2}>About diversity in organizations</h2>
-      <div className={Styles.container2}>
-        {inclusionDiversity.map((item, index) => (
-          <ContentCard
-            key={`diversity-${index}`}
-            title={item.title}
-            content={item.content}
-            imageSrc={item.imageSrc}
-          />
-        ))}
-      </div>
+      <div className={Styles.containerBox}>
+        <h2 className={Styles.titleTwo}>About inclusion at workplace</h2>
+        <div className={Styles.cardsContainer}>
+          {inclusionContent.map((item, index) => (
+            <ContentCard
+              key={`inclusion-${index}`}
+              title={item.title}
+              content={item.content}
+              imageSrc={item.imageSrc}
+            />
+          ))}
+        </div>
+      </div> 
 
-      <h2 className={Styles.title2}>About mental health at work</h2>
-      <div className={Styles.container3}>
-        {mentalHealth.map((item, index) => (
-          <ContentCard
-            key={`mental-health-${index}`}
-            title={item.title}
-            content={item.content}
-            imageSrc={item.imageSrc}
-          />
-        ))}
-      </div>
-    </>
+      <div className={Styles.containerBox}>
+        <h2 className={Styles.titleTwo}>About diversity in organizations</h2>
+        <div className={Styles.cardsContainer}>
+          {inclusionDiversity.map((item, index) => (
+            <ContentCard
+              key={`diversity-${index}`}
+              title={item.title}
+              content={item.content}
+              imageSrc={item.imageSrc}
+            />
+          ))}
+        </div>
+      </div> 
+
+      <div className={Styles.containerBox}> 
+        <h2 className={Styles.titleTwo}>About mental health at work</h2>
+        <div className={Styles.cardsContainer}>
+          {mentalHealth.map((item, index) => (
+            <ContentCard
+              key={`mental-health-${index}`}
+              title={item.title}
+              content={item.content}
+              imageSrc={item.imageSrc}
+            />
+          ))}
+        </div>
+      </div> 
+    </div>
   )
 }
 
